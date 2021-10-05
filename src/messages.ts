06ffb1800003de3da7bcf9bb8fca7f00dbd07858
memanot <menemty@gmail.com>
2021-10-05 19:51:18 +0300

    .

--- src/messages.ts
+++ src/messages.ts
@@ -1,4 +1,5 @@
 import { Context } from 'telegraf'
+import { Message } from 'telegraf/typings/core/types/typegram'
 
 export async function deleteMessage(ctx: Context) {
   try {
@@ -6,4 +7,21 @@ export async function deleteMessage(ctx: Context) {
   } catch (e) {
     return { error: e }
   }
+}
+
+export function extractEntities(message?: Message, type?: string) {
+  const { text = '', entities = [] } = { ...message }
+  const extractedEntities: string[] = []
+
+  entities
+    .filter((x) => x.type === type)
+    .forEach((ent) => {
+      let sliced = text.slice(ent.offset, ent.offset + ent.length)
+      if (type === 'url' && !sliced.startsWith('http')) {
+        sliced = `https://${sliced}`
+      }
+      extractedEntities.push(sliced)
+    })
+
+  return extractedEntities
 }
