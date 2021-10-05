4faf6534c90532f9f292f30f4efeee88e1f0a023
memanot <menemty@gmail.com>
2021-10-05 20:17:31 +0300

    .

--- src/messages.ts
+++ src/messages.ts
@@ -25,3 +25,15 @@ export function extractEntities(message?: Message, type?: string) {
 
   return extractedEntities
 }
+
+export async function ignoreOldMessageUpdates(ctx: Context, next: () => any) {
+  const { chat, from, updateType, message } = ctx
+  const time = new Date().getTime() / 1000
+  const isOldTime = message && time - message.date < 5 * 60
+
+  if (updateType !== 'message' || isOldTime) {
+    return next()
+  }
+
+  return from && chat && message && `Ignoring message from ${from.id} at ${chat.id} (${time}:${message.date})`
+}
